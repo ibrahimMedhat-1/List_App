9cdffbc7d40e6f260c78fad1ac7297d96eb380c8
ibrahimmedhat <doha.kroz0@gmail.com>
2023-06-13 06:22:56 +0300

    first

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/ibrahimmedhat/Downloads/fluttersdk"
+export "FLUTTER_ROOT=/Users/ibrahimmedhat/Downloads/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/ibrahimmedhat/StudioProjects/listapp"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
